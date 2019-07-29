c0d7654cb1881f4713c939c397ebd81fb9f642ff
Fabio Felippi <fabiofelippi@gmail.com>
2019-07-29 16:34:58 -0300

    Finalizado o arquivo tipos.rb

--- Ruby/tipos.rb
+++ Ruby/tipos.rb
@@ -3,3 +3,16 @@ preco = 4500.00
 quantidade = 10
 disponivel = true
 
+
+puts produto.class    #Imprime o tipo da variavel
+puts preco.class
+puts quantidade.class
+puts disponivel.class
+
+puts produto
+puts preco
+puts quantidade
+puts disponivel
+
+
+
