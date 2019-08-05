cf9523b53e20bfc576050b33dac29c6adb2b15ae
Fabio Felippi <fabiofelippi@gmail.com>
2019-08-05 09:13:38 -0300

    Additional arguments on the classes3.rb file

--- Ruby/classes3.rb
+++ Ruby/classes3.rb
@@ -1,14 +1,21 @@
 class Conta
-  attr_accessor :saldo
+  attr_accessor :saldo, :nome
+
+  def initialize(nome)
+    self.saldo = 30.0
+    self.nome = nome
+  end
 
   def deposita(valor)
     self.saldo += valor
     puts '1 - Depositando a quantia de ' + valor.to_s + ' reais.'
-    puts "2 - Depositando a quantia de #{valor} reais."
+    puts "2 - Depositando a quantia de #{valor} reais na conta de #{self.nome}."
   end
 end
 
-c = Conta.new
-c.saldo = 3.0
+c = Conta.new('Fernando')
 c.deposita(100.00)
+puts c.saldo
+
+c.deposita(10.00)
 puts c.saldo
