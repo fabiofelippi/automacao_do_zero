1aa7b6cef24ab1eedd10f2270d3a2143e724b5ae
Fabio Felippi <fabiofelippi@gmail.com>
2019-08-08 09:08:40 -0300

    Added automated script

---
+++ Ruby/capy/spec/hello_spec.rb
@@ -0,0 +1,6 @@
+describe 'Primeiro script de teste' do
+    it 'visitar a página' do
+        visit 'https://training-wheels-protocol.herokuapp.com'
+        expect(page.title).to eql 'Training Wheels Protocol'
+    end
+end
