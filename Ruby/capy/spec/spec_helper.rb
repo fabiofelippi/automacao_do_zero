75ad0935c5cef76066f1a66da2ef52f3b91da1d8
Fabio Felippi <fabiofelippi@gmail.com>
2019-08-13 15:05:12 -0300

    Continuing the course on Capybara

--- Ruby/capy/spec/spec_helper.rb
+++ Ruby/capy/spec/spec_helper.rb
@@ -20,6 +20,6 @@ end
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
-  config.default_max_wait_time = 15
+  config.default_max_wait_time = 5
   config.app_host = 'https://training-wheels-protocol.herokuapp.com'
 end
--- Ruby/capy/spec/upload_spec.rb
+++ Ruby/capy/spec/upload_spec.rb
@@ -2,13 +2,24 @@
 
 describe 'Upload de arquivo', :upload do
   before(:each) do
+    visit 'https://training-wheels-protocol.herokuapp.com/upload'
     @arquivo = Dir.pwd + '/spec/fixtures/file.txt'
     @imagem = Dir.pwd + '/spec/fixtures/avengers.png'
-    visit 'https://training-wheels-protocol.herokuapp.com/upload'
   end
 
   it 'upload com arquivo texto' do
     attach_file('file-upload', @arquivo)
+    # click_button('Upload') ==> Esta linha ou a de baixo funcionam da mesma maneira
+    click_button 'Upload'
+    div_arquivo = find('#uploaded-file')
+    expect(div_arquivo.text). to eql 'file.txt'
+  end
+
+  it 'upload de imagem' do
+    attach_file('file-upload', @imagem)
+    click_button 'Upload'
+    img = find('#new-image')
+    expect(img[:src]).to include '/uploads/avengers.png'
   end
 
   after(:each) do
