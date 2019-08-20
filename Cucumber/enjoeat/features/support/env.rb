e6cb26afa78f884b50c3ce1b51cdb38fd5275787
Fabio Felippi <fabiofelippi@gmail.com>
2019-08-20 10:42:10 -0300

    Linted files with Rubocop

--- Cucumber/enjoeat/features/support/env.rb
+++ Cucumber/enjoeat/features/support/env.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'capybara'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
-    config.app_host = 'http://localhost:3000/'
+  config.default_driver = :selenium_chrome
+  config.app_host = 'http://localhost:3000/'
 end
