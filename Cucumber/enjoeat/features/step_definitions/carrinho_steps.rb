f21eb0129f06ee439062368d0cfb0258cc3a08b0
Fabio Felippi <fabiofelippi@gmail.com>
2019-08-21 11:00:10 -0300

    Order implemented

--- Cucumber/enjoeat/features/step_definitions/carrinho_steps.rb
+++ Cucumber/enjoeat/features/step_definitions/carrinho_steps.rb
@@ -68,3 +68,34 @@ end
 Quando('eu limpo o meu carrinho') do
   @rest_page.cart.clean
 end
+
+# Fechar carrinho
+
+Dado("que adicionei os seguintes itens no carrinho:") do |table|
+  @product_list = table.hashes
+  steps %{
+    Quando eu adiciono todos os itens
+  }
+end
+
+Quando("eu fecho o meu carrinho") do
+  @rest_page.cart.close
+end
+
+Então("o valor total de itens deve ser igual a {string}") do |total_items|
+  expect(
+    @order_page.cost_shipping[0]
+  ).to have_text total_items
+end
+
+Então("o valor do frete deve ser igual a {string}") do |shipping|
+  expect(
+    @order_page.cost_shipping[1]
+  ).to have_text shipping
+end
+
+Então("o valor total da compra deve ser igual a {string}") do |total_cart|
+  expect(
+    @order_page.cost_shipping[2]
+  ).to have_text total_cart
+end
--- Cucumber/enjoeat/features/support/hooks.rb
+++ Cucumber/enjoeat/features/support/hooks.rb
@@ -4,8 +4,13 @@ Before do
   page.current_window.resize_to(1440, 900)
   @rest_list_page = RestaurantListPage.new
   @rest_page = RestaurantPage.new
+  @order_page = OrderPage.new
 end
 
 Before('@bread_bakery') do
   visit '/restaurants/bread-bakery/menu'
+end
+
+Before('@green_food') do
+  visit '/restaurants/green-food/menu'
 end
