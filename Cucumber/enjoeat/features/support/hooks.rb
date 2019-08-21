da3565698ca5aadfd4fdd248f18e988566e7a08c
Fabio Felippi <fabiofelippi@gmail.com>
2019-08-21 15:32:28 -0300

    Screenshots only for failed tests implemented

--- Cucumber/enjoeat/features/support/hooks.rb
+++ Cucumber/enjoeat/features/support/hooks.rb
@@ -16,8 +16,10 @@ Before('@green_food') do
   visit '/restaurants/green-food/menu'
 end
 
-After do
-  shot_file = page.save_screenshot("log/screenshot.png")
-  shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
-  embed(shot_b64, "image/png", "Screenshot")
+After do |scenario|
+  if scenario.failed?
+    shot_file = page.save_screenshot("log/screenshot.png")
+    shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
+    embed(shot_b64, "image/png", "Screenshot")
+  end
 end
