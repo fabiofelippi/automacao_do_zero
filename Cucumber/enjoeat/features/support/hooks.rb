4745443d08b8da44257c887fa791b68364607435
Fabio Felippi <fabiofelippi@gmail.com>
2019-08-21 15:31:38 -0300

    Screenshot reports implemented

--- Cucumber/enjoeat/features/support/hooks.rb
+++ Cucumber/enjoeat/features/support/hooks.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require "base64"
 
 Before do
   page.current_window.resize_to(1440, 900)
@@ -13,4 +14,10 @@ end
 
 Before('@green_food') do
   visit '/restaurants/green-food/menu'
+end
+
+After do
+  shot_file = page.save_screenshot("log/screenshot.png")
+  shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
+  embed(shot_b64, "image/png", "Screenshot")
 end
